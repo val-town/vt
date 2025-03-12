ae020057cbd7be86c506480cfdb9a53fed987b24
Wolf Mermelstein <wolf@404wolf.com>
2025-03-12 18:35:48 -0400

    Add deletion to push test case

--- src/vt/git/tests/push_test.ts
+++ src/vt/git/tests/push_test.ts
@@ -1,10 +1,10 @@
-import { clone } from "~/vt/git/clone.ts";
 import { doWithTempDir } from "~/vt/git/utils.ts";
 import { doWithNewProject } from "~/vt/git/tests/utils.ts";
 import sdk from "~/sdk.ts";
 import { push } from "~/vt/git/push.ts";
-import { assertEquals } from "@std/assert";
+import { assertEquals, assertRejects } from "@std/assert";
 import { join } from "@std/path";
+import ValTown from "@valtown/sdk";
 
 Deno.test({
   name: "test pushing",
@@ -50,6 +50,24 @@ Deno.test({
           .getContent(project.id, vtFilePath)
           .then((resp) => resp.text());
         assertEquals(newFileContent, "test2");
+
+        // Delete the file and push
+        await Deno.remove(localFilePath);
+        await push({
+          targetDir: tempDir,
+          projectId: project.id,
+          branchId: branch.id,
+          ignoreGlobs: [],
+        });
+
+        // Assert that the file no longer exists on the remote
+        await assertRejects(
+          async () => {
+            await sdk.projects.files.getContent(project.id, vtFilePath);
+          },
+          ValTown.APIError,
+          "404",
+        );
       }, "vt_push_test");
     });
   },
