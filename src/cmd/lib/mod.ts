dee6340b49a22cdb0f06b9ff53151aa66fbaf947
Wolf Mermelstein <wolf@404wolf.com>
2025-03-18 18:43:24 -0400

    Move all commands to cmd/lib

--- src/cmd/lib/mod.ts
+++ src/cmd/lib/mod.ts
@@ -1,4 +1,7 @@
-export { cloneCmd } from "./clone.ts";
-export { statusCmd } from "./status.ts";
 export { branchCmd } from "./branch.ts";
+export { browseCmd } from "./browse.ts";
 export { checkoutCmd } from "./checkout.ts";
+export { cloneCmd } from "./clone.ts";
+export { createCmd } from "./create.ts";
+export { statusCmd } from "./status.ts";
+export { watchCmd } from "./watch.ts";
