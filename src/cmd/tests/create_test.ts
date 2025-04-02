d4207bd71859ddfa64c354d74845d95b8421c243
Wolf Mermelstein <wolf@404wolf.com>
2025-04-01 21:38:32 -0400

    Improve creation tests

--- src/cmd/tests/create_test.ts
+++ src/cmd/tests/create_test.ts
@@ -6,7 +6,7 @@ import { cmd } from "~/cmd/root.ts";
 import sdk, { randomProjectName, user } from "~/sdk.ts";
 import { doWithTempDir } from "~/vt/lib/utils.ts";
 
-Deno.test("create new project and clone", async (c) => {
+Deno.test("create new project in specific directory", async (c) => {
   const newProjectName = randomProjectName();
   let newProject: ValTown.Projects.ProjectCreateResponse | null = null;
 
@@ -32,10 +32,83 @@ Deno.test("create new project and clone", async (c) => {
       });
     });
   } finally {
-    // Clean up -- delete the project
-    if (newProject) {
-      // @ts-ignore newProject.id is defined by here
-      await sdk.projects.delete(newProject.id);
-    }
+    // @ts-ignore newProject is defined but something went wrong
+    await sdk.projects.delete(newProject.id);
+  }
+});
+
+Deno.test("create new private project", async (c) => {
+  const newProjectName = randomProjectName();
+  let newProject: ValTown.Projects.ProjectCreateResponse | null = null;
+
+  try {
+    await doWithTempDir(async (tmpDir) => {
+      await c.step("create a new private project", async () => {
+        await cmd.parse(["create", newProjectName, "--private", tmpDir]);
+
+        newProject = await sdk.alias.username.projectName.retrieve(
+          user.username!,
+          newProjectName,
+        );
+
+        assertEquals(newProject.name, newProjectName);
+        assertEquals(newProject.author.username, user.username);
+        assertEquals(
+          newProject.privacy,
+          "private",
+          "project should be private",
+        );
+      });
+
+      await c.step("make sure the project is cloned", async () => {
+        assert(
+          await exists(join(tmpDir, newProjectName)),
+          "project was not cloned to target",
+        );
+      });
+    });
+  } finally {
+    // @ts-ignore newProject is defined but something went wrong
+    if (newProject) await sdk.projects.delete(newProject.id);
+  }
+});
+
+Deno.test("create new project in current working directory", async (c) => {
+  const newProjectName = randomProjectName();
+  let newProject: ValTown.Projects.ProjectCreateResponse | null = null;
+  const originalCwd = Deno.cwd;
+
+  try {
+    await doWithTempDir(async (tmpDir) => {
+      // Mock the cwd function to return the temp directory
+      Deno.cwd = () => tmpDir;
+
+      await c.step("create a new project in current directory", async () => {
+        await cmd.parse(["create", newProjectName]);
+
+        newProject = await sdk.alias.username.projectName.retrieve(
+          user.username!,
+          newProjectName,
+        );
+
+        assertEquals(newProject.name, newProjectName);
+        assertEquals(newProject.author.username, user.username);
+      });
+
+      await c.step(
+        "make sure the project is cloned to current directory",
+        async () => {
+          assert(
+            await exists(join(tmpDir, newProjectName)),
+            "project was not cloned to current directory",
+          );
+        },
+      );
+    });
+  } finally {
+    // Restore the original cwd function
+    Deno.cwd = originalCwd;
+    // @ts-ignore newProject is defined but something went wrong
+    if (newProject) await sdk.projects.delete(newProject.id);
   }
 });
