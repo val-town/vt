977b1bcc86a410819386e3bd3919289a150e5af2
Wolf Mermelstein <wolf@404wolf.com>
2025-04-23 00:45:17 -0400

    Graceful Copying (#116)
    * Make copying graceful
    
    * Graceful copying
    
    * Tick version
    
    * Absolute imports
    
    * Format code
    
    * Graceful deletions during checkout
    
    * Use graceful copy for pull
    
    * Make deletions graceful

--- src/cmd/tests/remix_test.ts
+++ src/cmd/tests/remix_test.ts
@@ -1,5 +1,5 @@
 import { doWithNewProject } from "~/vt/lib/tests/utils.ts";
-import { doWithTempDir } from "~/vt/lib/utils.ts";
+import { doWithTempDir } from "~/vt/lib/utils/misc.ts";
 import { join } from "@std/path";
 import sdk, { user } from "~/sdk.ts";
 import { runVtCommand } from "~/cmd/tests/utils.ts";
