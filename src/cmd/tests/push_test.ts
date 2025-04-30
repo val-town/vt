3a7cfe98c8f042d7923a2f3e691ec2e235bb4be0
Wolf Mermelstein <wolf@404wolf.com>
2025-04-30 16:52:11 -0400

    Fix push test

--- src/cmd/tests/push_test.ts
+++ src/cmd/tests/push_test.ts
@@ -65,8 +65,6 @@ Deno.test({
         });
 
         const fullPath = join(tmpDir, val.name);
-        await Deno.remove(join(fullPath, ".vtignore"));
-        await Deno.remove(join(fullPath, "deno.json"));
 
         await t.step("run push command with no changes", async () => {
           const [output] = await runVtCommand(["push"], fullPath);
