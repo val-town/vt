a838954f9e80c78e9566097579f53a9c434a0a8d
Wolf Mermelstein <wolf@404wolf.com>
2025-05-02 16:08:48 -0400

    Update scripts

--- companion/src/daemon.ts
+++ companion/src/daemon.ts
@@ -8,8 +8,14 @@ async function main() {
     .then((ws) => {
       ws.addEventListener("message", async (e) => {
         const link = JSON.parse(e.data).link as string;
-        console.log("Received link: ", link);
-        const allTabs = await chrome.tabs.query({});
+        const tab = (await chrome.tabs.query({}))
+          .find((t) => t.url === link);
+        if (tab) {
+          console.log("Tab already open, reloading...");
+          console.log(tab)
+          // Reload the tab if it is already open
+          await chrome.tabs.reload(tab.id!);
+        }
       });
     })
     .catch((err) =>
