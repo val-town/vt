d7dda7422cf9471e8889e8e54a87c212bd4d6de8
Wolf Mermelstein <wolf@404wolf.com>
2025-05-21 18:23:54 -0400

    Fix chrome extension context menu

--- companion/src/daemon/menu.ts
+++ companion/src/daemon/menu.ts
@@ -3,16 +3,19 @@ import { VTDaemon } from "./VTDaemon.js";
 
 export function setupContextMenu(daemon: VTDaemon) {
   browser.runtime.onInstalled.addListener(() => {
+    // Action for manifest v3, browser_action for manifest v2. Seems like it
+    // can't hurt to add both!
+
     chrome.contextMenus.create({
       id: "wakeUpDaemon",
       title: "Connect to VT CLI",
-      contexts: ["browser_action"],
+      contexts: ["browser_action", "action"],
     });
 
     chrome.contextMenus.create({
       id: "aboutMenu",
       title: "About VT Companion",
-      contexts: ["browser_action"],
+      contexts: ["browser_action", "action"],
     });
   });
 
