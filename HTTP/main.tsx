e427448659a5f9e5d079041a2ee1ee03413d80c8
Wolf Mermelstein <wolf@404wolf.com>
2025-08-12 10:05:15 -0400

    Remove http

--- HTTP/main.tsx
+++
@@ -1,5 +0,0 @@
-export default async function (req: Request): Promise<Response> {
-  console.log(req.headers);
-  // Learn more: https://docs.val.town/vals/http/
-  return Response.json({ ok: true });
-}
