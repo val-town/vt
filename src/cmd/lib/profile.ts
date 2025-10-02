e2055af139675e712e9ed493c457595096623097
Wolf Mermelstein <wolf@404wolf.com>
2025-10-01 20:20:21 -0400

    Clean up profile command

--- src/cmd/lib/profile.ts
+++ src/cmd/lib/profile.ts
@@ -8,19 +8,38 @@ import { ansi } from "@cliffy/ansi";
 export const profileCmd = new Command()
   .name("profile")
   .alias("me")
-  .description("Get information about the currently authenticated user")
+  .description("Get information about the currently authenticated user (you!)")
   .action(async () => {
-    await doWithSpinner("Creating new Val...", async (spinner) => {
+    await doWithSpinner("Loading profile information...", async (spinner) => {
       const user = await getCurrentUser();
+
+      // TODO: Use the async generator when we change this to `.list`
+      const orgNames: string[] = [];
+      let cursor = 0;
+      do {
+        const res = await sdk.orgs.retrieve({ limit: 100, offset: cursor });
+        res.data.map((o) => o.username).forEach((name) => orgNames.push(name));
+        cursor += res.data.length;
+      } while (orgNames.length < cursor && cursor < 1000);
+
       spinner.stop();
-      console.log(
-        `You're logged in as @${
-          ansi.link( // terminal code for link
-            colors.bold(user.username!),
-            user.url,
+
+      let currentlyLoggedInMsg = `You're logged in as @${
+        ansi.link( // terminal code for link
+          colors.bold(user.username!), // we made this non-null but it hasn't propagated
+          user.url,
+        )
+      }`;
+      if (orgNames.length > 0) {
+        currentlyLoggedInMsg += ` and a member of ${
+          colors.bold(
+            orgNames.map((n) => ansi.link(`@${n}`, orgUsernameToOrgLink(n)))
+              .join(", "),
           )
-        }`,
-      ); // usernames no longer can be null but the schema isn't updated yet
+        }`;
+      }
+      currentlyLoggedInMsg += ".";
+      console.log(currentlyLoggedInMsg);
 
       if (user.bio) {
         console.log(colors.dim(wrap(`"${user.bio}"`)));
@@ -28,7 +47,7 @@ export const profileCmd = new Command()
 
       if (user.tier === "pro") {
         console.log(
-          `You're on the ${colors.yellow(colors.bold("Pro"))} plan. 🚀`,
+          `Thank you for being a ${colors.yellow(colors.bold("Pro"))} member! 🚀`,
         );
       } else {
         console.log(
@@ -37,7 +56,9 @@ export const profileCmd = new Command()
           } plan. Head over to https://www.val.town/pricing to sign up for pro for custom domains, unlimited private Vals, and more!`,
         );
       }
-
-      const orgs = await sdk.
     });
   });
+
+function orgUsernameToOrgLink(orgUsername: string) {
+  return `https://www.val.town/orgs/${orgUsername}`;
+}
